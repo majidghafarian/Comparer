90d1a19be85d3928508ebccae3358bd7e2669945
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-04-26 23:09:47 +0330

    d

--- Domain/ProductDetail.cs
+++ Domain/ProductDetail.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain
@@ -11,5 +13,11 @@ namespace Domain
         public int Id { get; set; }
         public string Feature { get; set; }
         public string Value { get; set; }
+       
+        public int productid { get; set; }
+        [ForeignKey("productid")]
+        [JsonIgnore]
+        public Product? product { get; set; }
+
     }
 }
